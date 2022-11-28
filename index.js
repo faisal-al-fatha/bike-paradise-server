b40f790ab1d1e4c7fd5adef3e93edc1bcea3fc81
faisal-al-fatha <faysal313@gmail.com>
2022-11-28 15:40:17 +0600

    booking get api

--- index.js
+++ index.js
@@ -84,7 +84,16 @@ async function run(){
             res.send(result);
        
     });
+    app.get('/bookings', async(req, res)=> {
+        const email = req.query.email;
+        const query = {email: email};
+        const bookings = await bookingCollection.find(query).toArray();
+        res.send(bookings);
+    })
     app.post('/bookings', async(req, res)=> {
+        const email = req.query.email;
+        const query = {email: email};
+        const queryUser = await usersCollection.findOne(query);
             const booking = req.body;
             console.log(booking);
             const result = await bookingCollection.insertOne(booking);
